b38f3c1e8737b3b16d47f86fec4eb4fb5c1603f0
kuroneko0441 <kuroneko0441@gmail.com>
2017-03-23 03:50:51 +0900

    comment

--- Board.cpp
+++ Board.cpp
@@ -3,13 +3,16 @@
 
 double Board::evaluate(const int x, const int rotation) {
 	char**newBoard = getNewBoard(x, rotation);
+
+	/* position error: piece escapes board */
 	if(newBoard == nullptr) return -1000000;
 
 	int sum_top = 0;
 	int sum_hole = 0;
-	int complete = 0;
-	int bumpiness = 0;
+	int complete = 0;	/* completed line */
+	int bumpiness = 0;	/* absolute value of height difference */
 
+	/* saving new tops */
 	int* newTop = new int[boardW];
 	memset(newTop, 0, boardW * sizeof(int));
 	for(int x = 0; x < boardW; x++) {
@@ -17,21 +20,28 @@ double Board::evaluate(const int x, const int rotation) {
 			if(newBoard[y][x] == '1') { newTop[x] = (y + 1); break; }
 		}
 	}
+
+	/* counting holes */
 	for(int x = 0; x < boardW; x++) {
 		for(int y = 0; y < newTop[x]; y++) {
 			if(newBoard[y][x] == '0') { sum_hole++; }
 		}
 	}
 
-
+	/* counting tops */
 	for(int i = 0; i < boardW; i++) { sum_top += newTop[i]; }
 
+	/* counting complete lines */
 	for(int y = 0; y < boardH; y++) {
-		bool flag = true;
-		for(int x = 0; x < boardW; x++) { if(newBoard[y][x] == '0') flag = false; }
+		bool flag = true;	/* flag for line complete*/
+		for(int x = 0; x < boardW; x++) { 
+			/* breaks if line is not complete */
+			if(newBoard[y][x] == '0') { flag = false; break; }
+		}
 		if(flag == true) { complete++; }
 	}
 
+	/* counting bumpiness */
 	for(int i = 0; i < boardW - 1; i++) { bumpiness += abs(newTop[i] - newTop[i + 1]); }
 
 	if(newBoard != nullptr) {
@@ -39,11 +49,15 @@ double Board::evaluate(const int x, const int rotation) {
 		SAFE_DELETE(newBoard);
 	}
 	SAFE_DELETE(newTop);
+
+	/* weights from <Tetris AI - The (Near) Perfect Bot> */
 	return -0.510066*sum_top + 0.760666*complete + -0.35663*sum_hole + -0.184483*bumpiness;
 }
 
 char ** Board::getNewBoard(int x, int rotation) {
-	bool flag = true;
+	bool flag = true;	/* flag for possible X*/
+
+	/* new board after this piece falls */
 	char** newboard = new char*[boardH];
 	for(int i = 0; i < boardH; i++) {
 		newboard[i] = new char[boardW];
@@ -51,8 +65,7 @@ char ** Board::getNewBoard(int x, int rotation) {
 			newboard[i][j] = board[i][j];
 	}
 
-	/* 각 조각의 위치에 맞게 newBoard를 수정함 */
-	//TODO top 위치 y값 엑세스 위반
+	/* set 1 at newBoard, new position */
 	switch(curPiece) {
 	case 'O':
 		if(x < 1 ||
@@ -219,313 +232,317 @@ int * Board::select() {
 	int*best = new int[2];
 	best[0] = 0;
 	best[1] = 1;
-	//int min = boardH;
-	//int min_index = boardW;
 
-	//switch(curPiece) {
-	//case 'O':
-	//	//위치가 없을 때
-	//	for(int i = 0; i < boardW - 1; i++)
-	//		if(MAX2(top[i], top[i + 1]) <= min) {
-	//			min = MAX2(top[i], top[i + 1]);
-	//			min_index = i - 1;
-	//		}
-	//	min = boardH;
-	//	//깊숙한 모형 1
-	//	for(int i = 0; i < boardW - 1; i++)
-	//		if((i == 0 || top[i - 1] > (top[i] > top[i + 1] ? top[i] : top[i + 1])) &&
-	//			(i == boardW - 2 || top[i + 2] > (top[i] > top[i + 1] ? top[i] : top[i + 1])) &&
-	//		   (top[i] > top[i + 1] ? top[i] : top[i + 1]) + 1 <= min) {
-	//			min = (top[i] > top[i + 1] ? top[i] : top[i + 1]) + 1;
-	//			min_index = i - 1;
-	//		}
-	//	//2칸 공백 (모형 1)
-	//	for(int i = 0; i < boardW - 1; i++)
-	//		if(top[i] == top[i + 1] &&
-	//		   top[i] <= min) {
-	//			min = top[i];
-	//			min_index = i - 1;
-	//		}
-	//	break;
-	//case 'I':
-	//	//1칸 구멍
-	//	for(int i = 0; i < boardW; i++) {
-	//		if((top[i] + 1 == top[i + 1] || top[i] + 1 == top[i - 1]) &&
-	//		   top[i] <= min) {
-	//			min = top[i];
-	//			min_index = i - 2;
-	//			best[1] = 2;
-	//		}
-	//	}
-	//	//2칸 구멍
-	//	for(int i = 0; i < boardW; i++) {
-	//		if((top[i] + 2 == top[i + 1] || top[i] + 2 == top[i - 1]) &&
-	//		   top[i] <= min) {
-	//			min = top[i];
-	//			min_index = i - 2;
-	//			best[1] = 2;
-	//		}
-	//	}
-	//	//평면 (모형 1)
-	//	for(int i = 0; i < boardW - 3; i++)
-	//		if(top[i] == top[i + 1] &&
-	//		   top[i] == top[i + 2] &&
-	//		   top[i] == top[i + 3] &&
-	//		   top[i] <= min) {
-	//			min = top[i];
-	//			min_index = i;
-	//			best[1] = 1;
-	//		}
-	//	//3칸 이상 구멍
-	//	for(int i = 0; i < boardW; i++) {
-	//		if((top[i] + 3 <= top[i + 1] || top[i] + 3 <= top[i - 1]) &&
-	//		   top[i] <= min) {
-	//			min = top[i];
-	//			min_index = i - 2;
-	//			best[1] = 2;
-	//		}
-	//	}
-	//	break;
-	//case 'S':
-	//	//평평할때 모형 2
-	//	for(int i = 0; i < boardW - 1; i++)
-	//		if(top[i] > top[i + 1] &&
-	//		   top[i] <= min) {
-	//			min = top[i];
-	//			min_index = i - 2;
-	//			best[1] = 2;
-	//		}
-	//	min += 1;
-	//	//깊숙한 모형
-	//	//for(int i = 0; i<boardW - 1; i++)
-	//	//	if((i == 0 || top[i - 1] > top[i] + 2) &&
-	//	//		(i == boardW - 2 || top[i + 2] > top[i + 1] + 2) &&
-	//	//	   (top[i] > top[i + 1] ? top[i] : top[i + 1]) <= min) {
-	//	//		min = (top[i] > top[i + 1] ? top[i] : top[i + 1]);
-	//	//		min_index = i - 1;
-	//	//	}
-	//	//모형 2
-	//	for(int i = 0; i < boardW - 1; i++)
-	//		if(top[i + 1] == top[i] - 1 &&
-	//		   top[i + 1] <= min) {
-	//			min = top[i + 1];
-	//			min_index = i - 2;
-	//			best[1] = 2;
-	//		}
-	//	//모형 1
-	//	for(int i = 0; i < boardW - 2; i++)
-	//		if(top[i] == top[i + 1] &&
-	//		   top[i + 2] == top[i + 1] + 1 &&
-	//		   top[i] <= min) {
-	//			min = top[i];
-	//			min_index = i - 1;
-	//			best[1] = 1;
-	//		}
-	//	break;
-	//case 'Z':
-	//	//평평할때 모형 2
-	//	for(int i = 0; i < boardW - 1; i++)
-	//		if(top[i] < top[i + 1] &&
-	//		   top[i + 1] <= min) {
-	//			min = top[i + 1];
-	//			min_index = i - 2;
-	//			best[1] = 2;
-	//		}
-	//	min += 1;
-	//	//공백 있는 모형 1
-	//	//for(int i = 0; i < boardW - 2; i++)
-	//	//	if(top[i] == top[i + 2] + 1 &&
-	//	//	   top[i + 1] > top[i + 2] &&
-	//	//	   top[i + 2] <= min) {
-	//	//		min = top[i + 2];
-	//	//		min_index = i - 1;
-	//	//		best[1] = 1;
-	//	//	}
-	//	//깊숙한 모형 2
-	//	//for(int i=0;i<boardW-1;i++)
-	//	//	if((i == 0 || top[i - 1] - 1 > (top[i] > top[i + 1]) ? top[i] : top[i + 1]) &&
-	//	//		(i == board - 2 || top[i + 1] - 1 > (top[i] > top[i + 1]) ? top[i] : top[i + 1]) &&
-	//	//	   top[i] < min) {
-	//	//		min = top[i];
-	//	//		min_index = i - 2;
-	//	//		best[1] = 2;
-	//	//	}
-	//	//모형 2
-	//	for(int i = 0; i < boardW - 1; i++)
-	//		if(top[i + 1] == top[i] + 1 &&
-	//		   top[i] <= min) {
-	//			min = top[i];
-	//			min_index = i - 2;
-	//			best[1] = 2;
-	//		}
-	//	//모형 1
-	//	for(int i = 0; i < boardW - 2; i++)
-	//		if(top[i] == top[i + 1] + 1 &&
-	//		   top[i + 2] == top[i + 1] &&
-	//		   top[i + 1] <= min) {
-	//			min = top[i + 1];
-	//			min_index = i - 1;
-	//			best[1] = 1;
-	//		}
-	//	break;
-	//case 'L':
-	//	//깊숙한 모형 4
-	//	for(int i = 0; i < boardW - 1; i++)
-	//		if(top[i] > top[i + 1] + 2 &&
-	//			(i == boardW - 2 || top[i + 2] > top[i]) &&
-	//		   top[i + 1] <= min) {
-	//			min = top[i + 1];
-	//			min_index = i - 1;
-	//			best[1] = 4;
-	//		}
-	//	min += 1;
-	//	//2칸 평면 (모형 2)
-	//	for(int i = 0; i < boardW - 1; i++)
-	//		if(top[i] == top[i + 1] &&
-	//		   top[i] <= min) {
-	//			min = top[i];
-	//			min_index = i - 2;
-	//			best[1] = 2;
-	//		}
-	//	//3칸 평면 (모형 3)
-	//	for(int i = 0; i < boardW - 2; i++)
-	//		if(top[i] == top[i + 1] &&
-	//		   top[i + 2] == top[i + 1] &&
-	//		   top[i] <= min) {
-	//			min = top[i];
-	//			min_index = i - 1;
-	//			best[1] = 3;
-	//		}
-	//	//모형 4
-	//	for(int i = 0; i < boardW - 1; i++)
-	//		if(top[i] == top[i + 1] + 2 &&
-	//		   top[i + 1] <= min) {
-	//			min = top[i + 1];
-	//			min_index = i - 1;
-	//			best[1] = 4;
-	//		}
-	//	//모형 1
-	//	for(int i = 0; i < boardW - 2; i++)
-	//		if(top[i] == top[i + 1] - 1 &&
-	//		   top[i + 2] == top[i + 1] &&
-	//		   top[i] <= min) {
-	//			min = top[i];
-	//			min_index = i - 1;
-	//			best[1] = 1;
-	//		}
-	//	break;
-	//case 'J':
-	//	//깊숙한 모형 2
-	//	for(int i = 0; i < boardW - 1; i++)
-	//		if(top[i + 1] > top[i] + 2 &&
-	//			(i == 0 || top[i - 1] > top[i + 1]) &&
-	//		   top[i] <= min) {
-	//			min = top[i];
-	//			min_index = i - 2;
-	//			best[1] = 2;
-	//		}
-	//	min += 1;
-	//	//모형 4
-	//	for(int i = 0; i < boardW - 1; i++)
-	//		if(top[i] == top[i + 1] &&
-	//		   top[i] <= min) {
-	//			min = top[i];
-	//			min_index = i - 1;
-	//			best[1] = 4;
-	//		}
-	//	//모형 3
-	//	for(int i = 0; i < boardW - 2; i++)
-	//		if(top[i] == top[i + 1] &&
-	//		   top[i + 2] == top[i + 1] &&
-	//		   top[i] <= min) {
-	//			min = top[i];
-	//			min_index = i - 1;
-	//			best[1] = 3;
-	//		}
-	//	//모형 1
-	//	for(int i = 0; i < boardW - 2; i++)
-	//		if(top[i] == top[i + 1] &&
-	//		   top[i + 2] == top[i + 1] - 1 &&
-	//		   top[i + 2] <= min) {
-	//			min = top[i + 2];
-	//			min_index = i - 1;
-	//			best[1] = 1;
-	//		}
-	//	//모형 2
-	//	for(int i = 0; i < boardW - 1; i++)
-	//		if(top[i + 1] == top[i] + 2 &&
-	//		   top[i] <= min) {
-	//			min = top[i];
-	//			min_index = i - 2;
-	//			best[1] = 2;
-	//		}
-	//	break;
-	//case 'T':
-	//	//모형 2
-	//	for(int i = 0; i < boardW - 1; i++)
-	//		if(top[i + 1] == top[i] + 1 &&
-	//		   top[i] <= min) {
-	//			min = top[i];
-	//			min_index = i - 2;
-	//			best[1] = 2;
-	//		}
-	//	//깊숙한 모형 2
-	//	//for(int i = 0; i < boardW - 1; i++)
-	//	//	if(top[i + 1] > top[i] + 4 &&
-	//	//	   (i==0||top[i-1]>top[i+1]) &&
-	//	//	   (i==boardW-2||top[i+2>top[i+1]]) &&
-	//	//	   top[i]+1 <= min) {
-	//	//		min = top[i]+1;
-	//	//		min_index = i - 2;
-	//	//		best[1] = 2;
-	//	//	}
-	//	//모형 4
-	//	for(int i = 0; i < boardW - 1; i++)
-	//		if(top[i] == top[i + 1] + 1 &&
-	//		   top[i] <= min) {
-	//			min = top[i + 1];
-	//			min_index = i - 1;
-	//			best[1] = 4;
-	//		}
-	//	//모형 3
-	//	for(int i = 0; i < boardW - 2; i++)
-	//		if(top[i] == top[i + 1] &&
-	//		   top[i + 2] == top[i + 1] &&
-	//		   top[i] <= min) {
-	//			min = top[i];
-	//			min_index = i - 1;
-	//			best[1] = 3;
-	//		}
-	//	//깊숙한 모형 1
-	//	for(int i = 0; i < boardW - 2; i++)
-	//		if(top[i] >= top[i + 1] + 2 &&
-	//		   top[i + 2] == top[i] &&
-	//		   top[i + 1] <= min - 4) {
-	//			min = top[i + 1];
-	//			min_index = i - 1;
-	//			best[1] = 1;
-	//		}
-	//	//모형 1
-	//	for(int i = 0; i < boardW - 2; i++)
-	//		if(top[i] == top[i + 1] + 1 &&
-	//		   top[i + 2] == top[i] &&
-	//		   top[i + 1] <= min) {
-	//			min = top[i + 1];
-	//			min_index = i - 1;
-	//			best[1] = 1;
-	//		}
-	//	break;
-	//default:
-	//	min_index = 7;
-	//	break;
-	//}
+	/* unusing codes -  rule based */
+	{
+		//int min = boardH;
+		//int min_index = boardW;
 
-	//best[0] = min_index;
+		//switch(curPiece) {
+		//case 'O':
+		//	//위치가 없을 때
+		//	for(int i = 0; i < boardW - 1; i++)
+		//		if(MAX2(top[i], top[i + 1]) <= min) {
+		//			min = MAX2(top[i], top[i + 1]);
+		//			min_index = i - 1;
+		//		}
+		//	min = boardH;
+		//	//깊숙한 모형 1
+		//	for(int i = 0; i < boardW - 1; i++)
+		//		if((i == 0 || top[i - 1] > (top[i] > top[i + 1] ? top[i] : top[i + 1])) &&
+		//			(i == boardW - 2 || top[i + 2] > (top[i] > top[i + 1] ? top[i] : top[i + 1])) &&
+		//		   (top[i] > top[i + 1] ? top[i] : top[i + 1]) + 1 <= min) {
+		//			min = (top[i] > top[i + 1] ? top[i] : top[i + 1]) + 1;
+		//			min_index = i - 1;
+		//		}
+		//	//2칸 공백 (모형 1)
+		//	for(int i = 0; i < boardW - 1; i++)
+		//		if(top[i] == top[i + 1] &&
+		//		   top[i] <= min) {
+		//			min = top[i];
+		//			min_index = i - 1;
+		//		}
+		//	break;
+		//case 'I':
+		//	//1칸 구멍
+		//	for(int i = 0; i < boardW; i++) {
+		//		if((top[i] + 1 == top[i + 1] || top[i] + 1 == top[i - 1]) &&
+		//		   top[i] <= min) {
+		//			min = top[i];
+		//			min_index = i - 2;
+		//			best[1] = 2;
+		//		}
+		//	}
+		//	//2칸 구멍
+		//	for(int i = 0; i < boardW; i++) {
+		//		if((top[i] + 2 == top[i + 1] || top[i] + 2 == top[i - 1]) &&
+		//		   top[i] <= min) {
+		//			min = top[i];
+		//			min_index = i - 2;
+		//			best[1] = 2;
+		//		}
+		//	}
+		//	//평면 (모형 1)
+		//	for(int i = 0; i < boardW - 3; i++)
+		//		if(top[i] == top[i + 1] &&
+		//		   top[i] == top[i + 2] &&
+		//		   top[i] == top[i + 3] &&
+		//		   top[i] <= min) {
+		//			min = top[i];
+		//			min_index = i;
+		//			best[1] = 1;
+		//		}
+		//	//3칸 이상 구멍
+		//	for(int i = 0; i < boardW; i++) {
+		//		if((top[i] + 3 <= top[i + 1] || top[i] + 3 <= top[i - 1]) &&
+		//		   top[i] <= min) {
+		//			min = top[i];
+		//			min_index = i - 2;
+		//			best[1] = 2;
+		//		}
+		//	}
+		//	break;
+		//case 'S':
+		//	//평평할때 모형 2
+		//	for(int i = 0; i < boardW - 1; i++)
+		//		if(top[i] > top[i + 1] &&
+		//		   top[i] <= min) {
+		//			min = top[i];
+		//			min_index = i - 2;
+		//			best[1] = 2;
+		//		}
+		//	min += 1;
+		//	//깊숙한 모형
+		//	//for(int i = 0; i<boardW - 1; i++)
+		//	//	if((i == 0 || top[i - 1] > top[i] + 2) &&
+		//	//		(i == boardW - 2 || top[i + 2] > top[i + 1] + 2) &&
+		//	//	   (top[i] > top[i + 1] ? top[i] : top[i + 1]) <= min) {
+		//	//		min = (top[i] > top[i + 1] ? top[i] : top[i + 1]);
+		//	//		min_index = i - 1;
+		//	//	}
+		//	//모형 2
+		//	for(int i = 0; i < boardW - 1; i++)
+		//		if(top[i + 1] == top[i] - 1 &&
+		//		   top[i + 1] <= min) {
+		//			min = top[i + 1];
+		//			min_index = i - 2;
+		//			best[1] = 2;
+		//		}
+		//	//모형 1
+		//	for(int i = 0; i < boardW - 2; i++)
+		//		if(top[i] == top[i + 1] &&
+		//		   top[i + 2] == top[i + 1] + 1 &&
+		//		   top[i] <= min) {
+		//			min = top[i];
+		//			min_index = i - 1;
+		//			best[1] = 1;
+		//		}
+		//	break;
+		//case 'Z':
+		//	//평평할때 모형 2
+		//	for(int i = 0; i < boardW - 1; i++)
+		//		if(top[i] < top[i + 1] &&
+		//		   top[i + 1] <= min) {
+		//			min = top[i + 1];
+		//			min_index = i - 2;
+		//			best[1] = 2;
+		//		}
+		//	min += 1;
+		//	//공백 있는 모형 1
+		//	//for(int i = 0; i < boardW - 2; i++)
+		//	//	if(top[i] == top[i + 2] + 1 &&
+		//	//	   top[i + 1] > top[i + 2] &&
+		//	//	   top[i + 2] <= min) {
+		//	//		min = top[i + 2];
+		//	//		min_index = i - 1;
+		//	//		best[1] = 1;
+		//	//	}
+		//	//깊숙한 모형 2
+		//	//for(int i=0;i<boardW-1;i++)
+		//	//	if((i == 0 || top[i - 1] - 1 > (top[i] > top[i + 1]) ? top[i] : top[i + 1]) &&
+		//	//		(i == board - 2 || top[i + 1] - 1 > (top[i] > top[i + 1]) ? top[i] : top[i + 1]) &&
+		//	//	   top[i] < min) {
+		//	//		min = top[i];
+		//	//		min_index = i - 2;
+		//	//		best[1] = 2;
+		//	//	}
+		//	//모형 2
+		//	for(int i = 0; i < boardW - 1; i++)
+		//		if(top[i + 1] == top[i] + 1 &&
+		//		   top[i] <= min) {
+		//			min = top[i];
+		//			min_index = i - 2;
+		//			best[1] = 2;
+		//		}
+		//	//모형 1
+		//	for(int i = 0; i < boardW - 2; i++)
+		//		if(top[i] == top[i + 1] + 1 &&
+		//		   top[i + 2] == top[i + 1] &&
+		//		   top[i + 1] <= min) {
+		//			min = top[i + 1];
+		//			min_index = i - 1;
+		//			best[1] = 1;
+		//		}
+		//	break;
+		//case 'L':
+		//	//깊숙한 모형 4
+		//	for(int i = 0; i < boardW - 1; i++)
+		//		if(top[i] > top[i + 1] + 2 &&
+		//			(i == boardW - 2 || top[i + 2] > top[i]) &&
+		//		   top[i + 1] <= min) {
+		//			min = top[i + 1];
+		//			min_index = i - 1;
+		//			best[1] = 4;
+		//		}
+		//	min += 1;
+		//	//2칸 평면 (모형 2)
+		//	for(int i = 0; i < boardW - 1; i++)
+		//		if(top[i] == top[i + 1] &&
+		//		   top[i] <= min) {
+		//			min = top[i];
+		//			min_index = i - 2;
+		//			best[1] = 2;
+		//		}
+		//	//3칸 평면 (모형 3)
+		//	for(int i = 0; i < boardW - 2; i++)
+		//		if(top[i] == top[i + 1] &&
+		//		   top[i + 2] == top[i + 1] &&
+		//		   top[i] <= min) {
+		//			min = top[i];
+		//			min_index = i - 1;
+		//			best[1] = 3;
+		//		}
+		//	//모형 4
+		//	for(int i = 0; i < boardW - 1; i++)
+		//		if(top[i] == top[i + 1] + 2 &&
+		//		   top[i + 1] <= min) {
+		//			min = top[i + 1];
+		//			min_index = i - 1;
+		//			best[1] = 4;
+		//		}
+		//	//모형 1
+		//	for(int i = 0; i < boardW - 2; i++)
+		//		if(top[i] == top[i + 1] - 1 &&
+		//		   top[i + 2] == top[i + 1] &&
+		//		   top[i] <= min) {
+		//			min = top[i];
+		//			min_index = i - 1;
+		//			best[1] = 1;
+		//		}
+		//	break;
+		//case 'J':
+		//	//깊숙한 모형 2
+		//	for(int i = 0; i < boardW - 1; i++)
+		//		if(top[i + 1] > top[i] + 2 &&
+		//			(i == 0 || top[i - 1] > top[i + 1]) &&
+		//		   top[i] <= min) {
+		//			min = top[i];
+		//			min_index = i - 2;
+		//			best[1] = 2;
+		//		}
+		//	min += 1;
+		//	//모형 4
+		//	for(int i = 0; i < boardW - 1; i++)
+		//		if(top[i] == top[i + 1] &&
+		//		   top[i] <= min) {
+		//			min = top[i];
+		//			min_index = i - 1;
+		//			best[1] = 4;
+		//		}
+		//	//모형 3
+		//	for(int i = 0; i < boardW - 2; i++)
+		//		if(top[i] == top[i + 1] &&
+		//		   top[i + 2] == top[i + 1] &&
+		//		   top[i] <= min) {
+		//			min = top[i];
+		//			min_index = i - 1;
+		//			best[1] = 3;
+		//		}
+		//	//모형 1
+		//	for(int i = 0; i < boardW - 2; i++)
+		//		if(top[i] == top[i + 1] &&
+		//		   top[i + 2] == top[i + 1] - 1 &&
+		//		   top[i + 2] <= min) {
+		//			min = top[i + 2];
+		//			min_index = i - 1;
+		//			best[1] = 1;
+		//		}
+		//	//모형 2
+		//	for(int i = 0; i < boardW - 1; i++)
+		//		if(top[i + 1] == top[i] + 2 &&
+		//		   top[i] <= min) {
+		//			min = top[i];
+		//			min_index = i - 2;
+		//			best[1] = 2;
+		//		}
+		//	break;
+		//case 'T':
+		//	//모형 2
+		//	for(int i = 0; i < boardW - 1; i++)
+		//		if(top[i + 1] == top[i] + 1 &&
+		//		   top[i] <= min) {
+		//			min = top[i];
+		//			min_index = i - 2;
+		//			best[1] = 2;
+		//		}
+		//	//깊숙한 모형 2
+		//	//for(int i = 0; i < boardW - 1; i++)
+		//	//	if(top[i + 1] > top[i] + 4 &&
+		//	//	   (i==0||top[i-1]>top[i+1]) &&
+		//	//	   (i==boardW-2||top[i+2>top[i+1]]) &&
+		//	//	   top[i]+1 <= min) {
+		//	//		min = top[i]+1;
+		//	//		min_index = i - 2;
+		//	//		best[1] = 2;
+		//	//	}
+		//	//모형 4
+		//	for(int i = 0; i < boardW - 1; i++)
+		//		if(top[i] == top[i + 1] + 1 &&
+		//		   top[i] <= min) {
+		//			min = top[i + 1];
+		//			min_index = i - 1;
+		//			best[1] = 4;
+		//		}
+		//	//모형 3
+		//	for(int i = 0; i < boardW - 2; i++)
+		//		if(top[i] == top[i + 1] &&
+		//		   top[i + 2] == top[i + 1] &&
+		//		   top[i] <= min) {
+		//			min = top[i];
+		//			min_index = i - 1;
+		//			best[1] = 3;
+		//		}
+		//	//깊숙한 모형 1
+		//	for(int i = 0; i < boardW - 2; i++)
+		//		if(top[i] >= top[i + 1] + 2 &&
+		//		   top[i + 2] == top[i] &&
+		//		   top[i + 1] <= min - 4) {
+		//			min = top[i + 1];
+		//			min_index = i - 1;
+		//			best[1] = 1;
+		//		}
+		//	//모형 1
+		//	for(int i = 0; i < boardW - 2; i++)
+		//		if(top[i] == top[i + 1] + 1 &&
+		//		   top[i + 2] == top[i] &&
+		//		   top[i + 1] <= min) {
+		//			min = top[i + 1];
+		//			min_index = i - 1;
+		//			best[1] = 1;
+		//		}
+		//	break;
+		//default:
+		//	min_index = 7;
+		//	break;
+		//}
+
+		//best[0] = min_index;
+	}
 
 	double bestQ = -1000000;
-	for(int i = 0; i < boardW; i++) {
-		for(int j = 1; j < 5; j++) {
-			double Q = evaluate(i, j);
+	for(int i = 0; i < boardW; i++) {	/* for every Xs */
+		for(int j = 1; j < 5; j++) {	/* for every rotations */
+			double Q = evaluate(i, j);	/* find Max Q */
 			if(Q > bestQ) {
 				bestQ = Q;
 				best[0] = i;
@@ -538,6 +555,7 @@ int * Board::select() {
 
 Board::Board(const char board[], const int boardW, const int boardH, const int curPiece, const int nextPiece)
 	:boardW(boardW), boardH(boardH), curPiece(curPiece), nextPiece(nextPiece) {
+	/* convert 1d board to 2d board */
 	this->board = new char*[boardH];
 	for(int y = 0; y < boardH; y++) {
 		this->board[y] = new char[boardW];
@@ -545,6 +563,7 @@ Board::Board(const char board[], const int boardW, const int boardH, const int c
 			this->board[y][x] = board[y*boardW + x];
 	}
 
+	/* saving tops */
 	this->top = new int[boardW];
 	memset(top, 0, boardW * sizeof(int));
 	for(int i = 0; i < boardH; i++)
@@ -552,6 +571,7 @@ Board::Board(const char board[], const int boardW, const int boardH, const int c
 			if(board[i*boardW + j] == '1')
 				top[j] = (i + 1);
 
+	/* saving holes */
 	this->hole = new int[boardW];
 	memset(hole, 0, boardW * sizeof(int));
 	for(int x = 0; x < boardW; x++)
